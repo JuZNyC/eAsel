986a763e4667e686670b16b7776a9df80efb6b26
Leeran <leeran7@gmail.com>
2021-11-23 16:57:00 -0500

    added sample login form and dependencies with auth0, added Auth0Provider, no errors

--- client/src/index.js
+++ client/src/index.js
@@ -3,8 +3,19 @@ import ReactDOM from 'react-dom';
 // import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Auth0Provider } from '@auth0/auth0-react';
+const DOMAIN = process.env.REACT_APP_VARIABLE_DOMAIN;
+const CLIENTID = process.env.REACT_APP_VARIABLE_CLIENTID;
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+<Auth0Provider
+    domain={DOMAIN}
+    clientId={CLIENTID}
+    redirectUri={window.location.origin}
+>
+    <App />
+</Auth0Provider>
+, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
